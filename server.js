5b038d033630fcaaad060c5ac0aac35faaa42f0d
Jessica Chen <jchen@jchen.attlocal.net>
2019-04-11 21:01:37 -0500

    adjusted to use handlebars

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ var mongoose = require("mongoose");
 var axios = require("axios");
 var cheerio = require("cheerio");
 
+// Handlebars
+var exphbs = require("express-handlebars");
+
+
 // Require all models
 var db = require("./models");
 
@@ -17,8 +21,12 @@ var app = express();
 // Parse request body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+// Setting up Handlebars
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 // Make public a static folder
 app.use(express.static("public"));
+
 
 // Start the server
 app.listen(PORT, function() {
