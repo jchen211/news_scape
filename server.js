b201626e2aebeaadba8df8bf40fb7d766a035119
Jessica Chen <jchen@jchen.attlocal.net>
2019-04-11 20:53:46 -0500

    initial files + pkg.json added

---
+++ server.js
@@ -0,0 +1,27 @@
+var express = require("express");
+var mongoose = require("mongoose");
+
+var axios = require("axios");
+var cheerio = require("cheerio");
+
+// Require all models
+var db = require("./models");
+
+var PORT = 3000;
+
+// Initialize Express
+var app = express();
+
+// Configure middleware
+
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Make public a static folder
+app.use(express.static("public"));
+
+// Start the server
+app.listen(PORT, function() {
+    console.log("App running on port " + PORT + "!");
+  });
+  
